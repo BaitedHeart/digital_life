9da202ed8e15538a3b31ed8b2519187a00c4d88f
BaitedHeart <dominixen@gmail.com>
2026-09-20 21:07:50 +0200

    main.py aktualisieren

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 print(“DIGITAL LIFE // SYSTEM START”)
+egg = "Digi-Ei"
+print(egg)
+hatch_progress = 0
+
+while hatch_progress < 100:
+    hatch_progress = hatch_progress + 10
+    print(hatch_progress)
+
+if hatch_progress >= 100:
+    print("Das Digi-Ei schlüpft!")
