c14b709f55182fb5b038ab7646dd7167efcb40bc
BaitedHeart <dominixen@gmail.com>
2026-09-20 22:46:18 +0200

    main.py aktualisieren
    Add interactive hatch loop

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 print(“DIGITAL LIFE // SYSTEM START”)
 egg = "Digi-Ei"
 print(egg)
+
 hatch_progress = 0
 
 while hatch_progress < 100:
-    hatch_progress = hatch_progress + 10
-    print(hatch_progress)
+    action = input("Berühre das Digi-Ei? (ja/nein): ")
+
+    if action == "ja":
+        hatch_progress = hatch_progress + 10
+        print("Schlüpf-Fortschritt:", hatch_progress, "%")
+    else:
+        print("Das Digi-Ei wartet...")
+
 
 if hatch_progress >= 100:
     print("Das Digi-Ei schlüpft!")
