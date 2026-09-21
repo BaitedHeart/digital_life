5e2e03eef7ed0051a918c4d6d84fe920095f64d1
BaitedHeart <dominixen@gmail.com>
2026-09-21 08:42:03 +0200

    main.py aktualisieren
    Add bond and warmth development values

--- main.py
+++ main.py
@@ -1,18 +1,35 @@
-print(“DIGITAL LIFE // SYSTEM START”)
+print("DIGITAL LIFE // SYSTEM START")
+
 egg = "Digi-Ei"
 print(egg)
 
 hatch_progress = 0
+bond = 0
+warmth = 0
 
 while hatch_progress < 100:
-    action = input("Berühre das Digi-Ei? (ja/nein): ")
+    action = input("Aktion wählen (berühren/wärmen/warten): ")
 
-    if action == "ja":
+    if action == "berühren":
         hatch_progress = hatch_progress + 10
+        bond = bond + 1
         print("Schlüpf-Fortschritt:", hatch_progress, "%")
-    else:
+
+    elif action == "wärmen":
+        hatch_progress = hatch_progress + 20
+        warmth = warmth + 1
+        print("Schlüpf-Fortschritt:", hatch_progress, "%")
+
+    elif action == "warten":
         print("Das Digi-Ei wartet...")
 
+    else:
+        print("Unbekannte Aktion.")
 
 if hatch_progress >= 100:
     print("Das Digi-Ei schlüpft!")
+
+    if bond >= 5:
+        print("Das Wesen reagiert sofort auf deine Nähe.")
+    else:
+        print("Das Wesen wirkt noch etwas zurückhaltend.")
